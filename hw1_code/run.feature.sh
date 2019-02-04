75938cc17389c8a238fcb93179b157e779f4c7a0
Prajwal Prakash Vasisht <prajwalppv@gmail.com>
2019-02-04 10:51:40 -0500

    fixed bug

--- hw1_code/run.feature.sh
+++ hw1_code/run.feature.sh
@@ -16,7 +16,7 @@ video_path=~/video   # path to the directory containing all the videos. In this 
 cluster_num=400        # the number of clusters in k-means. Note that 50 is by no means the optimal solution.
                       # You need to explore the best config by yourself.
 batch_size=10000
-feat_dir=kmeans/
+feat_dir=kmeans_norm/
 mkdir -p audio mfcc kmeans
 
 # This part does feature extraction, it may take quite a while if you have a lot of videos. Totally 3 steps are taken:
